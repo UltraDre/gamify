76739ad4a649c6200562bebf0c15014aecd30646
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2024-02-13 15:10:57 +0100

    feat: added code for result page

--- src/components/general/QuizResult.tsx
+++ src/components/general/QuizResult.tsx
@@ -1,11 +1,28 @@
+"use client";
+import { useRouter } from "next/navigation";
 import * as React from "react";
 
 interface IQuizResultProps {}
 
 const QuizResult: React.FunctionComponent<IQuizResultProps> = (props) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    router.push("/");
+  };
+
   return (
-    <div>
-      <p>Quiz Result</p>
+    <div className="w-full min-h-screen bg-light flex items-center justify-center flex-col gap-y-5">
+      <p className="text-5xl text-center font-semibold">congratulations</p>
+      <p className="text-3xl capitalize font-medium">Saheeb olalekan</p>
+      <p className="text-xl">Matric no: 15/69/0096</p>
+      <p className="text-2xl">you've completed Quiz ladder for this week</p>
+      <button
+        className="bg-black text-light px-6 py-3 rounded-md text-2xl"
+        onClick={handleLogout}
+      >
+        logout
+      </button>
     </div>
   );
 };
