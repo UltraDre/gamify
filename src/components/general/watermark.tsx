95e6371bd48a97ac63978d3896a12e7bb7d5173d
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2024-03-13 01:03:46 +0100

    Fix: name increased

--- src/components/general/watermark.tsx
+++ src/components/general/watermark.tsx
@@ -6,13 +6,13 @@ const Watermark: React.FunctionComponent<IWatermarkProps> = (props) => {
   return (
     <div className="absolute inset-0">
       <div className="left-10 top-10 absolute">
-        <p className="text-xl font-medium">
+        <p className="text-5xl font-medium">
           Gamification in smart classroom BY OLALEKAN SAHEEB Q. 15/69/0096
         </p>
       </div>
 
       <div className="right-10 bottom-10 absolute">
-        <p className="text-xl font-medium">
+        <p className="text-5xl font-medium">
           Gamification in smart classroom BY OLALEKAN SAHEEB Q. 15/69/0096
         </p>
       </div>
